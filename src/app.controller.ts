86ed49c5d6e12b0a853e12b78c66f81a9efc340b
Urwah Sajjad <usajjad@hotmail.co.uk>
2024-01-05 04:15:38 +0300

    logging exception on error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -13,6 +13,11 @@ export class AppController {
 
   @Post('/reply')
   async chatReply(@Body() body) {
-    return await this.appService.getUserReply(body.chat);
+    try {
+      return await this.appService.getUserReply(body.chat);
+    } catch (err) {
+      console.error(err.message)
+      throw new HttpException(`error on user reply ${err.message}`, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
   }
 }
