a94cb7b98c934b0846c552698aac56ca96fb30ab
Urwah Sajjad <usajjad@hotmail.co.uk>
2024-01-23 22:35:56 +0300

    formatted code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,37 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { logToSlack } from './slack.log';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello()
+    return this.appService.getHello();
   }
 
   @Post('/reply')
   async chatReply(@Body() body) {
-    console.log("body", body)
-    logToSlack(`request receieved with body: ${JSON.stringify(body, null, 2)}`)
-    this.appService.getUserReply(body.chat, body.contact?.id).catch(err => {
-      console.error(err.message)
-      logToSlack(err.message)
-      throw new HttpException(`error on user reply ${err.message}`, HttpStatus.INTERNAL_SERVER_ERROR)
-    })
+    console.log('body', body);
+    logToSlack(`request receieved with body: ${JSON.stringify(body, null, 2)}`);
+    this.appService.getUserReply(body.chat, body.contact?.id).catch((err) => {
+      console.error(err.message);
+      logToSlack(err.message);
+      throw new HttpException(
+        `error on user reply ${err.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    });
 
-    
-    return 'OK'
+    return 'OK';
   }
 }
